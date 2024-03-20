23115840c0a4924bfc72ddce5025b13efbccad51
sarthakk21 <sarthakk281102@gmail.com>
2024-03-20 19:15:12 +0530

    Finalised

--- Assignment 1/halo.c
+++ Assignment 1/halo.c
@@ -1,7 +1,6 @@
 #include "mpi.h"
 #include <stdio.h>
 #include <stdlib.h>
-// #include <math.h>
 
 int main(int argc, char *argv[]) {
     MPI_Init(&argc, &argv);
@@ -19,27 +18,24 @@ int main(int argc, char *argv[]) {
     }
 
     int Px = atoi(argv[1]); // Number of processes in the x-direction
-    int N = atoi(argv[2]); // Total no of data points square per process
+    int N = atoi(argv[2]);  // Size of the data matrix
 
     // Newton-Raphson method to calculate the square root of N
     int sqrt1 = N / 2;  
     int temp = 0;  
  
-    while (sqrt1 != temp) // Initially temp is 0 and sqrt = num  
-    {  
-        temp = sqrt1; // assign sqrt to temp  
-          
+    while (sqrt1 != temp){  
+        temp = sqrt1;
         sqrt1 = ( N / temp + temp) / 2;  
     }
 
-    int N_side = sqrt1; // Side length of the data points square
-    int num_time_steps = atoi(argv[3]);
-    int seed = atoi(argv[4]);
-    int stencil = atoi(argv[5]); // Stencil type: 5-point or 9-point
+    int N_side = sqrt1;                      // Assuming N is a perfect square
+    int num_time_steps = atoi(argv[3]);      // Number of time steps
+    int seed = atoi(argv[4]);                // Seed for random number generator
+    int stencil = atoi(argv[5]);             // 5-point or 9-point
 
-    // Initialize data points with seed
     srand(seed * (rank + 10));
-    // Create the data matrix
+    // Creating the data matrix
     double** data = (double**)malloc(N_side * sizeof(double*));
     for (double i = 0; i < N_side; i++){
         data[(int)i] = (double*)malloc(N_side * sizeof(double));
@@ -50,7 +46,6 @@ int main(int argc, char *argv[]) {
         }
     }
 
-    // Assuming a simple 2D grid of processes (Px x Py)
     int Py = size / Px;
     if (Px * Py != size) {
         if (rank == 0) printf("Error: Px * Py does not match the number of processes.\n");
@@ -58,35 +53,35 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
-    // Calculate position in the grid
+    // Calculating position of the process in the processes grid
     int x = rank % Px;
     int y = rank / Px;
 
-    // Flags to check for neighbors
+    // Which neighbours exist
     int has_top = y > 0;
     int has_bottom = y < (Py - 1);
     int has_left = x > 0;
     int has_right = x < (Px - 1);
 
 
-    MPI_Request requests[4]; // For non-blocking communication
+    MPI_Request requests[4];
     int request_count = 0;
 
     MPI_Status status[4];
     int status_count = 0;
 
     double sTime, eTime;
-    sTime = MPI_Wtime();
+    sTime = MPI_Wtime();   // Start the timer
 
     for (int step = 0; step < num_time_steps; step++) {
         if(stencil == 5) {
-            // 5-point stencil
             double top_buff_send[N_side], top_buff_recv[N_side];
             double bottom_buff_send[N_side], bottom_buff_recv[N_side];
             double left_buff_send[N_side], left_buff_recv[N_side]; 
             double right_buff_send[N_side], right_buff_recv[N_side];
             double top[N_side], bottom[N_side], left[N_side], right[N_side];
             int position;
+
             // Pack and send to top neighbor
             if (has_top) {
                 position = 0;
@@ -100,8 +95,8 @@ int main(int argc, char *argv[]) {
             if (has_top) {
                 position = 0;
                 MPI_Recv(top_buff_recv, N_side*(sizeof(double)), MPI_PACKED, rank - Px, rank - Px, MPI_COMM_WORLD, &status[status_count++]);
-                // Wait for all communications to complete
-                MPI_Waitall(request_count, requests, status); // Check this later!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+                
+                MPI_Waitall(request_count, requests, status); // Waiting for all communications to complete
                 for(int i = 0; i < N_side; i++){
                     MPI_Unpack(top_buff_recv, N_side*(sizeof(double)), &position, top+i, 1, MPI_DOUBLE, MPI_COMM_WORLD);
                 }
@@ -120,8 +115,8 @@ int main(int argc, char *argv[]) {
             if (has_bottom) {
                 position = 0;
                 MPI_Recv(bottom_buff_recv, N_side*(sizeof(double)), MPI_PACKED, rank + Px, rank + Px, MPI_COMM_WORLD, &status[status_count++]);
-                // Wait for all communications to complete
-                MPI_Waitall(request_count, requests, status); // Check this later!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+                
+                MPI_Waitall(request_count, requests, status); // Waiting for all communications to complete
                 for(int i = 0; i < N_side; i++){
                     MPI_Unpack(bottom_buff_recv, N_side*(sizeof(double)), &position, bottom+i, 1, MPI_DOUBLE, MPI_COMM_WORLD);
                 }
@@ -140,8 +135,8 @@ int main(int argc, char *argv[]) {
             if (has_left) {
                 position = 0;
                 MPI_Recv(left_buff_recv, N_side*(sizeof(double)), MPI_PACKED, rank - 1, rank - 1, MPI_COMM_WORLD, &status[status_count++]);
-                // Wait for all communications to complete
-                MPI_Waitall(request_count, requests, status); // Check this later!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+                MPI_Waitall(request_count, requests, status); // Waiting for all communications to complete
                 for(int i = 0; i < N_side; i++){
                     MPI_Unpack(left_buff_recv, N_side*(sizeof(double)), &position, left+i, 1, MPI_DOUBLE, MPI_COMM_WORLD);
                 }
@@ -160,8 +155,8 @@ int main(int argc, char *argv[]) {
             if (has_right) {
                 position = 0;
                 MPI_Recv(right_buff_recv, N_side*(sizeof(double)), MPI_PACKED, rank + 1, rank + 1, MPI_COMM_WORLD, &status[status_count++]);
-                // Wait for all communications to complete
-                MPI_Waitall(request_count, requests, status); // Check this later!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+            
+                MPI_Waitall(request_count, requests, status); // Waiting for all communications to complete
                 for(int i = 0; i < N_side; i++){
                     MPI_Unpack(right_buff_recv, N_side*(sizeof(double)), &position, right+i, 1, MPI_DOUBLE, MPI_COMM_WORLD);
                 }
@@ -216,6 +211,7 @@ int main(int argc, char *argv[]) {
                 data[N_side-1][N_side-1] = (data[N_side-1][N_side-1] + bottom[N_side-1] + data[N_side-2][N_side-1] + data[N_side-1][N_side-2] + right[N_side-1]) / 5.0;
             }
 
+            // Update the edge points
             if(!has_top){
                 for(int i = 1; i < N_side - 1; i++){
                     data[0][i] = (data[0][i] + data[1][i] + data[0][i-1] + data[0][i+1]) / 4.0;
@@ -239,7 +235,8 @@ int main(int argc, char *argv[]) {
                     data[i][N_side-1] = (data[i][N_side-1] + data[i+1][N_side-1] + data[i-1][N_side-1] + data[i][N_side-2]) / 4.0;
                 }
             }
-
+                
+            // Update the corner points
             if(!has_top && !has_left){
                 data[0][0] = (data[0][0] + data[1][0] + data[0][1]) / 3.0;
             }
@@ -288,12 +285,11 @@ int main(int argc, char *argv[]) {
                 data[N_side-1][N_side-1] = (data[N_side-1][N_side-1] + data[N_side-2][N_side-1] + right[N_side-1] + data[N_side-1][N_side-2]) / 4.0;
             }
 
-            // Reset request count for next iteration
+            // Reset request and status count for next iteration
             request_count = 0;
             status_count = 0;
         }
         else if(stencil == 9){
-            // 9-point stencil
             double top_buff_send[2*N_side], top_buff_recv[2*N_side];
             double bottom_buff_send[2*N_side], bottom_buff_recv[2*N_side];
             double left_buff_send[2*N_side], left_buff_recv[2*N_side];
@@ -317,8 +313,8 @@ int main(int argc, char *argv[]) {
             if (has_top) {
                 position = 0;
                 MPI_Recv(top_buff_recv, 2*N_side*(sizeof(double)), MPI_PACKED, rank - Px, rank - Px, MPI_COMM_WORLD, &status[status_count++]);
-                // Wait for all communications to complete
-                MPI_Waitall(request_count, requests, status);
+                
+                MPI_Waitall(request_count, requests, status);   // Waiting for all communications to complete
                 for(int i = 0; i < N_side; i++){
                     MPI_Unpack(top_buff_recv, 2*N_side*(sizeof(double)), &position, top+i, 1, MPI_DOUBLE, MPI_COMM_WORLD);
                 }
@@ -343,8 +339,8 @@ int main(int argc, char *argv[]) {
             if (has_bottom) {
                 position = 0;
                 MPI_Recv(bottom_buff_recv, 2*N_side*(sizeof(double)), MPI_PACKED, rank + Px, rank + Px, MPI_COMM_WORLD, &status[status_count++]);
-                // Wait for all communications to complete
-                MPI_Waitall(request_count, requests, status);
+    
+                MPI_Waitall(request_count, requests, status);    // Waiting for all communications to complete
                 for(int i = 0; i < N_side; i++){
                     MPI_Unpack(bottom_buff_recv, 2*N_side*(sizeof(double)), &position, bottom+i, 1, MPI_DOUBLE, MPI_COMM_WORLD);
                 }
@@ -369,8 +365,8 @@ int main(int argc, char *argv[]) {
             if (has_left) {
                 position = 0;
                 MPI_Recv(left_buff_recv, 2*N_side*(sizeof(double)), MPI_PACKED, rank - 1, rank - 1, MPI_COMM_WORLD, &status[status_count++]);
-                // Wait for all communications to complete
-                MPI_Waitall(request_count, requests, status);
+
+                MPI_Waitall(request_count, requests, status);    // Waiting for all communications to complete
                 for(int i = 0; i < N_side; i++){
                     MPI_Unpack(left_buff_recv, 2*N_side*(sizeof(double)), &position, left+i, 1, MPI_DOUBLE, MPI_COMM_WORLD);
                 }
@@ -395,8 +391,8 @@ int main(int argc, char *argv[]) {
             if (has_right) {
                 position = 0;
                 MPI_Recv(right_buff_recv, 2*N_side*(sizeof(double)), MPI_PACKED, rank + 1, rank + 1, MPI_COMM_WORLD, &status[status_count++]);
-                // Wait for all communications to complete
-                MPI_Waitall(request_count, requests, status);
+
+                MPI_Waitall(request_count, requests, status);    // Waiting for all communications to complete
                 for(int i = 0; i < N_side; i++){
                     MPI_Unpack(right_buff_recv, 2*N_side*(sizeof(double)), &position, right+i, 1, MPI_DOUBLE, MPI_COMM_WORLD);
                 }
@@ -469,6 +465,7 @@ int main(int argc, char *argv[]) {
                 data[N_side-2][N_side-2] = (data[N_side-2][N_side-2] + bottom[N_side-2] + data[N_side-1][N_side-2] + data[N_side-3][N_side-2] + data[N_side-4][N_side-2] + data[N_side-2][N_side-1] + data[N_side-2][N_side-3] + data[N_side-2][N_side-4] + right[N_side-2]) / 9.0;
             }
 
+            // Update the edge points
             if(!has_top){
                 for(int i = 2; i < N_side - 2; i++){
                     data[0][i] = (data[0][i] + data[1][i] + data[0][i-1] + data[0][i+1] + data[0][i-2] + data[0][i+2] + data[2][i]) / 7.0;
@@ -497,6 +494,7 @@ int main(int argc, char *argv[]) {
                 }
             }
 
+            // Update the corner points
             if(!has_top && !has_left){
                 data[0][0] = (data[0][0] + data[1][0] + data[0][1] + data[0][2] + data[2][0]) / 5.0;
                 data[1][0] = (data[1][0] + data[0][0] + data[2][0] + data[1][1] + data[1][2] + data[3][0]) / 6.0;
@@ -581,16 +579,17 @@ int main(int argc, char *argv[]) {
                 data[N_side-2][1] = (data[N_side-2][1] + data[N_side-1][1] + data[N_side-3][1] + data[N_side-2][0] + data[N_side-2][2] + data[N_side-4][1] + left[N_side-2] + data[N_side-2][3]) / 8.0;
             }
 
-            // Reset request count for next iteration
+            // Reset request and status count for next iteration
             request_count = 0;
             status_count = 0;
         }
     }
     
-    eTime = MPI_Wtime();
-    double time = eTime - sTime;
+    eTime = MPI_Wtime();    // End timer
+    double time = eTime - sTime;    
     double max_time;
-    MPI_Reduce(&time, &max_time, 1, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);
+    MPI_Reduce(&time, &max_time, 1, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);    // Get the maximum time taken by any process
+    
     if (rank == 0) {
         printf("Time taken for computing %d-point stencil: %f\n", stencil, max_time);
     }
--- Assignment 1/plot.py
+++ Assignment 1/plot.py
@@ -3,19 +3,16 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-# Define the data sizes (squared values) and stencil configurations
-data_sizes = [512**2, 2048**2]  # Data sizes
-stencil_configs = ['5', '9']  # Stencil configurations
+data_sizes = [512**2, 2048**2] 
+stencil_configs = ['5', '9']
 
-# Placeholder for actual time values
 actual_time_values = {
-    (512**2, '5'): [0.051235, 0.04091, 0.052509],  # Example values; replace with your actual data
+    (512**2, '5'): [0.051235, 0.04091, 0.052509], 
     (512**2, '9'): [0.059266, 0.074726, 0.066472],
     (2048**2, '5'): [0.463102, 0.555919, 0.3548],
     (2048**2, '9'): [0.697287, 0.594133, 0.627878],
 }
 
-# Generate a DataFrame with the actual execution times
 data = []
 for size in data_sizes:
     for config in stencil_configs:
@@ -25,7 +22,6 @@ for size in data_sizes:
 df = pd.DataFrame(data, columns=['Data Size', 'Stencil Configuration', 'Time (s)'])
 df['Size, Config'] = df.apply(lambda row: f"{int(np.sqrt(row['Data Size']))}^2, {row['Stencil Configuration']}", axis=1)
 
-# First plot: Original times
 plt.figure(figsize=(14, 8))
 sns.boxplot(x='Size, Config', y='Time (s)', data=df)
 plt.title('Time Taken by Main Halo Exchange Function for Various Data Sizes and Stencil Configurations')
@@ -34,4 +30,4 @@ plt.ylabel('Time (seconds)')
 plt.xlabel('(Data Size, Stencil Configuration)')
 plt.grid(True)
 plt.tight_layout()
-plt.savefig('E:\Sem6\CS633\CS633\Assignment 1\plot.png')  # Save the first plot
+plt.savefig('E:\Sem6\CS633\CS633\Assignment 1\plot.png')
